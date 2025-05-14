6d0052588c5b40650ac747f23e2d313aa764dfcf
ukkokalevala <148769692+ukkokalevala@users.noreply.github.com>
2025-05-14 10:29:44 +0200

    Create secrets.h

---
+++ secrets.h
@@ -0,0 +1,15 @@
+#ifndef SECRETS_H
+#define SECRETS_H
+
+// WiFi credentials
+const char* ssid = "HERE ";
+const char* password = "HERE ";
+
+// API Key
+const String API_KEY = "HERE ";
+
+// Latitude & Longitude
+const String LAT = "HERE ";     // Cape Town
+const String LON = "HERE ";
+
+#endif
